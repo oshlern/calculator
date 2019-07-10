8655d17e6ce6f9a5679c5d1ad0ee22b71f2c59e3
Osher Lerner <osherler@gmail.com>
2019-07-09 23:32:47 -0400

    sample java project

---
+++ calculator/src/main/java/com/oshlern/calculator/App.java
@@ -0,0 +1,13 @@
+package com.oshlern.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
